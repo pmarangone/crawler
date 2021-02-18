66b5970172d0de7e59b6f0397b863cc5cc1011a2
Sergei Kononov <57736527+hemulens@users.noreply.github.com>
2021-02-18 16:35:00 +0100

    fix: Disabling screen resize for Linux after GUI initialization.

--- src/app.cpp
+++ src/app.cpp
@@ -3,8 +3,11 @@
 // wxWidgets entry point: hands over control of main to wxWidgets
 // Called upon wxWidgets startup and should be used to initialize the program
 bool CrawlerApp::OnInit() {
-  MainFrame *frame = new MainFrame(GUI::appTitle, GUI::windowPosition, GUI::windowSize, GUI::windowStyle);
+  MainFrame *frame = new MainFrame(GUI::appTitle, GUI::windowPosition, GUI::windowSize, GUI::windowStyleNonResizable);
   frame->Show(true);
+  // Setting min & max size (to force no-resize option for Linux & Windows)
+  frame->SetMinSize(GUI::windowSize);
+  frame->SetMaxSize(GUI::windowSize);
   return true;
 }
 
