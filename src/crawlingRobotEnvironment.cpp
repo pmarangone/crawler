5f9f290973e542ce3fc358ed9c1117975c54f0ce
pmarangone <patrickmrds@hotmail.com>
2021-03-12 20:14:10 -0300

    chore: remove comments

--- src/crawlingRobotEnvironment.cpp
+++ src/crawlingRobotEnvironment.cpp
@@ -23,7 +23,6 @@ CrawlingRobotEnvironment::CrawlingRobotEnvironment(std::shared_ptr<CrawlingRobot
 
 CrawlingRobotEnvironment::~CrawlingRobotEnvironment() {}
 std::pair<int, int> CrawlingRobotEnvironment::GetCurrentState() {
-  std::cout << "_state = {" << _state.first << ", " << _state.second << "}" << std::endl;
   return _state;
 }
 
